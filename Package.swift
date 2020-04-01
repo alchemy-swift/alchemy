98b8536706f308a29a49667687371ac4562c1a28
Josh Wright <joshuawright11@gmail.com>
2020-03-31 17:41:02 -0700

    Package description

--- Package.swift
+++ Package.swift
@@ -10,27 +10,31 @@ let package = Package(
         .iOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Alchemy",
-            targets: ["Alchemy"]),
+            targets: ["Alchemy"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/codable-kit.git", .branch("master")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Alchemy",
             dependencies: [
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "CodableKit", package: "codable-kit"),
             ]
         ),
-        .target(name: "_Example", dependencies: ["Alchemy"]),
+        .target(
+            name: "_Example",
+            dependencies: ["Alchemy"]
+        ),
         .testTarget(
             name: "AlchemyTests",
-            dependencies: ["Alchemy"]),
+            dependencies: ["Alchemy"]
+        ),
     ]
 )
