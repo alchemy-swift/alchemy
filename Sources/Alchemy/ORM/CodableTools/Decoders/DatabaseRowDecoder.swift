e02896a416b6b2bcf92dbcd23cc69937712ffa38
Josh Wright <joshuawright11@gmail.com>
2020-05-11 14:48:49 -0700

    Fix decoding enums

--- Sources/Alchemy/ORM/CodableTools/Decoders/DatabaseRowDecoder.swift
+++ Sources/Alchemy/ORM/CodableTools/Decoders/DatabaseRowDecoder.swift
@@ -8,7 +8,6 @@ struct DatabaseRowDecoder: Decoder {
     let keyMappingStrategy: DatabaseKeyMappingStrategy
     
     init(row: DatabaseRow, keyMappingStrategy: DatabaseKeyMappingStrategy) {
-        print("Got columns: \(row.allColumns)")
         self.row = row
         self.keyMappingStrategy = keyMappingStrategy
     }
@@ -18,11 +17,30 @@ struct DatabaseRowDecoder: Decoder {
     }
     
     func unkeyedContainer() throws -> UnkeyedDecodingContainer {
+        /// This is for arrays, which we currently support in other ways.
         throw DatabaseDecodingError("This shouldn't be called; top level is keyed.")
     }
     
     func singleValueContainer() throws -> SingleValueDecodingContainer {
         throw DatabaseDecodingError("This shouldn't be called; top level is keyed.")
+    }
+}
+
+private struct DatabaseFieldDecoder: Decoder {
+    var codingPath: [CodingKey] = []
+    var userInfo: [CodingUserInfoKey : Any] = [:]
+    let field: DatabaseField
+    
+    func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
+        throw DatabaseDecodingError("`container` shouldn't be called; this is only for single values.")
+    }
+    
+    func unkeyedContainer() throws -> UnkeyedDecodingContainer {
+        throw DatabaseDecodingError("`unkeyedContainer` shouldn't be called; this is only for single values.")
+    }
+    
+    func singleValueContainer() throws -> SingleValueDecodingContainer {
+        _SingleValueDecodingContainer(field: self.field)
     }
 }
 
@@ -59,110 +77,161 @@ private struct KeyedContainer<Key: CodingKey>: KeyedDecodingContainerProtocol {
     /// Is the key nil?
     func decodeNil(forKey key: Key) throws -> Bool {
         print("Decode nil")
-        return try self.row.getField(columnName: self.string(for: key)).value.isNil
+        let isNil = try self.row.getField(columnName: self.string(for: key)).value.isNil
+        print("'\(key.stringValue)' was nil? \(isNil)")
+        return isNil
     }
     
     func decode(_ type: Bool.Type, forKey key: Key) throws -> Bool {
-        print("Bool from \(key.stringValue)")
         return try self.row.getField(columnName: self.string(for: key)).bool()
     }
     
     func decode(_ type: String.Type, forKey key: Key) throws -> String {
-        print("String from \(key.stringValue)")
         return try self.row.getField(columnName: self.string(for: key)).string()
     }
     
     func decode(_ type: Double.Type, forKey key: Key) throws -> Double {
-        print("Double from \(key.stringValue)")
         return try self.row.getField(columnName: self.string(for: key)).double()
     }
     
     func decode(_ type: Float.Type, forKey key: Key) throws -> Float {
-        print("Float from \(key.stringValue)")
         return Float(try self.row.getField(columnName: self.string(for: key)).double())
     }
     
     func decode(_ type: Int.Type, forKey key: Key) throws -> Int {
-        print("Int from \(key.stringValue)")
         return try self.row.getField(columnName: self.string(for: key)).int()
     }
     
     func decode(_ type: Int8.Type, forKey key: Key) throws -> Int8 {
-        print("Int8 from \(key.stringValue)")
         return Int8(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: Int16.Type, forKey key: Key) throws -> Int16 {
-        print("Int16 from \(key.stringValue)")
         return Int16(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: Int32.Type, forKey key: Key) throws -> Int32 {
-        print("Int32 from \(key.stringValue)")
         return Int32(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: Int64.Type, forKey key: Key) throws -> Int64 {
-        print("Int64 from \(key.stringValue)")
         return Int64(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: UInt.Type, forKey key: Key) throws -> UInt {
-        print("UInt from \(key.stringValue)")
         return UInt(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: UInt8.Type, forKey key: Key) throws -> UInt8 {
-        print("UInt8 from \(key.stringValue)")
         return UInt8(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: UInt16.Type, forKey key: Key) throws -> UInt16 {
-        print("UInt16 from \(key.stringValue)")
         return UInt16(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: UInt32.Type, forKey key: Key) throws -> UInt32 {
-        print("UInt32 from \(key.stringValue)")
         return UInt32(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode(_ type: UInt64.Type, forKey key: Key) throws -> UInt64 {
-        print("UInt64 from \(key.stringValue)")
         return UInt64(try self.row.getField(columnName: self.string(for: key)).int())
     }
     
     func decode<T>(_ type: T.Type, forKey key: Key) throws -> T where T : Decodable {
-        print("T from \(key.stringValue)")
-        print("val: \(try self.row.getField(columnName: "price_status").string())")
         if type == UUID.self {
             return try self.row.getField(columnName: self.string(for: key)).uuid() as! T
         } else if type == Date.self {
             return try self.row.getField(columnName: self.string(for: key)).date() as! T
         } else {
-            /// TODO: Use this for eager loading?
-            print("key: \(self.string(for: key)), type: \(Swift.type(of: type))")
-            throw DatabaseDecodingError("Nested database types aren't available, yet.")
+            let field = try self.row.getField(columnName: self.string(for: key))
+            return try T(from: DatabaseFieldDecoder(field: field))
         }
     }
     
     func nestedContainer<NestedKey>(keyedBy type: NestedKey.Type, forKey key: Key) throws -> KeyedDecodingContainer<NestedKey> where NestedKey : CodingKey {
-        print("Nested container")
         throw DatabaseDecodingError("This shouldn't be called? nextedContainer")
     }
     
     func nestedUnkeyedContainer(forKey key: Key) throws -> UnkeyedDecodingContainer {
-        print("Nested unkeyed")
         throw DatabaseDecodingError("This shouldn't be called? nestedUnkeyedContainer")
     }
     
     func superDecoder() throws -> Decoder {
-        print("Super decoder")
         throw DatabaseDecodingError("This shouldn't be called? superDecoder")
     }
     
     func superDecoder(forKey key: Key) throws -> Decoder {
-        print("Super decoder for key")
         throw DatabaseDecodingError("This shouldn't be called? superDecoder(forKey:)")
+    }
+}
+
+private struct _SingleValueDecodingContainer: SingleValueDecodingContainer {
+    var codingPath: [CodingKey] = []
+    
+    let field: DatabaseField
+    
+    func decodeNil() -> Bool {
+        self.field.value.isNil
+    }
+    
+    func decode(_ type: Bool.Type) throws -> Bool {
+        try self.field.bool()
+    }
+    
+    func decode(_ type: String.Type) throws -> String {
+        try self.field.string()
+    }
+    
+    func decode(_ type: Double.Type) throws -> Double {
+        try self.field.double()
+    }
+    
+    func decode(_ type: Float.Type) throws -> Float {
+        Float(try self.field.double())
+    }
+    
+    func decode(_ type: Int.Type) throws -> Int {
+        try self.field.int()
+    }
+    
+    func decode(_ type: Int8.Type) throws -> Int8 {
+        Int8(try self.field.int())
+    }
+    
+    func decode(_ type: Int16.Type) throws -> Int16 {
+        Int16(try self.field.int())
+    }
+    
+    func decode(_ type: Int32.Type) throws -> Int32 {
+        Int32(try self.field.int())
+    }
+    
+    func decode(_ type: Int64.Type) throws -> Int64 {
+        Int64(try self.field.int())
+    }
+    
+    func decode(_ type: UInt.Type) throws -> UInt {
+        UInt(try self.field.int())
+    }
+    
+    func decode(_ type: UInt8.Type) throws -> UInt8 {
+        UInt8(try self.field.int())
+    }
+    
+    func decode(_ type: UInt16.Type) throws -> UInt16 {
+        UInt16(try self.field.int())
+    }
+    
+    func decode(_ type: UInt32.Type) throws -> UInt32 {
+        UInt32(try self.field.int())
+    }
+    
+    func decode(_ type: UInt64.Type) throws -> UInt64 {
+        UInt64(try self.field.int())
+    }
+    
+    func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
+        throw PostgresError("Decode a type from a single value container is not supported.")
     }
 }
