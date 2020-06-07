0a19bb72f98aad375105bc67a7583be7fecb13c9
Josh Wright <joshuawright11@gmail.com>
2020-06-06 23:13:10 -0700

    Cleanup logger, add scheduled at timing.

--- Sources/Alchemy/Core/Log.swift
+++ Sources/Alchemy/Core/Log.swift
@@ -8,34 +8,31 @@ public struct Log {
     
     private static let logger = Logger(label: "inc.alchemy.your_app")
     
-    // An optional prefix for all logs. (will remove later, useful for debugging for now)
-    public static var prefix: String? = "[ Alchemy ] "
-    
     public static func trace(_ message: String) {
-        Log.logger.trace(.init(stringLiteral: (prefix ?? "") + "[ TRACE ] " + message))
+        Log.logger.trace(.init(stringLiteral: message))
     }
     
     public static func debug(_ message: String) {
-        Log.logger.debug(.init(stringLiteral: (prefix ?? "") + "[ DEBUG ] " + message))
+        Log.logger.debug(.init(stringLiteral: message))
     }
     
     public static func info(_ message: String) {
-        Log.logger.info(.init(stringLiteral: (prefix ?? "") + "[ INFO ] " + message))
+        Log.logger.info(.init(stringLiteral: message))
     }
     
     public static func notice(_ message: String) {
-        Log.logger.notice(.init(stringLiteral: (prefix ?? "") + "[ NOTICE ]" + message))
+        Log.logger.notice(.init(stringLiteral: message))
     }
     
     public static func warning(_ message: String) {
-        Log.logger.warning(.init(stringLiteral: (prefix ?? "") + "[ WARNING ] " + message))
+        Log.logger.warning(.init(stringLiteral: message))
     }
     
     public static func error(_ message: String) {
-        Log.logger.error(.init(stringLiteral: (prefix ?? "") + "[ ERROR ] " + message))
+        Log.logger.error(.init(stringLiteral: message))
     }
     
     public static func critical(_ message: String) {
-        Log.logger.critical(.init(stringLiteral: (prefix ?? "") + "[ CRITICAL ] " + message))
+        Log.logger.critical(.init(stringLiteral: message))
     }
 }
--- Sources/Alchemy/Jobs/Scheduler.swift
+++ Sources/Alchemy/Jobs/Scheduler.swift
@@ -18,10 +18,15 @@ public struct Scheduler: FactoryService {
     private var scheduleLoop: EventLoop
     
     @discardableResult
-    public func schedule<J: Job>(_ job: J, every schedule: Frequency) -> Scheduler {
+    public func schedule<J: Job>(_ job: J, every frequency: Frequency) -> Scheduler {
+        let secondsUntilFirst = frequency.initialDelay.nanoseconds / 1000000000
+        let seconds = secondsUntilFirst % 60
+        let minutes = (secondsUntilFirst / 60) % 60
+        let hours = (secondsUntilFirst / 60 / 60) % 24
+        Log.info("`\(name(of: J.self))` was scheduled. The first one will run in \(hours) hrs \(minutes) mins \(seconds) secs.")
         /// A single loop will do all the scheduling for now.
         self.scheduleLoop
-            .scheduleRepeatedTask(initialDelay: .seconds(0), delay: schedule.time) { repeatedTask in
+            .scheduleRepeatedTask(initialDelay: frequency.initialDelay, delay: frequency.time) { repeatedTask in
                 Log.info("Starting Job `\(name(of: J.self))`.")
                 // for now, never cancel the task.
                 _ = self.group.next()
@@ -38,45 +43,80 @@ public struct Scheduler: FactoryService {
 }
 
 public protocol Frequency {
+    /// The frequency @ which this should be repeated.
     var time: TimeAmount { get }
-    /// Ignore for now.
+    
+    /// The delay (from now) that the first of these Jobs should be scheduled.
+    ///
+    /// i.e. if the `Frequency` is hourly on the hour and it's 1:35, this will be equivalent to
+    /// `.minutes(25)`.
     var initialDelay: TimeAmount { get }
 }
 
 extension Frequency {
-    public var initialDelay: TimeAmount { .seconds(0) }
+    func timeUntilNext(day: Int? = nil, hr: Int? = nil, min: Int? = nil, sec: Int? = nil) -> TimeAmount {
+        let now = Date()
+        let calendar = Calendar.current
+        let components = DateComponents(calendar: calendar, day: day, hour: hr, minute: min, second: sec)
+        let nextTime = calendar.nextDate(after: now, matching: components, matchingPolicy: .nextTime)!
+        let interval = Int64(nextTime.timeIntervalSince(now) * 1000)
+        return .milliseconds(interval)
+    }
 }
 
 public struct DayFrequency: Frequency {
     let value: Int64
     
+    let hr: Int
+    let min: Int
+    let sec: Int
+
     public var time: TimeAmount { .hours(value * 24) }
     
+    public var initialDelay: TimeAmount {
+        self.timeUntilNext(hr: self.hr, min: self.min, sec: self.sec)
+    }
+    
     public func at(hr: Int = 0, min: Int = 0, sec: Int = 0) -> DayFrequency {
-        self
+        .init(value: self.value, hr: hr, min: min, sec: sec)
     }
 }
 
 public struct HourFrequency: Frequency {
     let value: Int64
+    
+    let min: Int
+    let sec: Int
+    
     public var time: TimeAmount { .hours(value) }
     
+    public var initialDelay: TimeAmount {
+        self.timeUntilNext(min: self.min, sec: self.sec)
+    }
+    
     public func at(min: Int = 0, sec: Int = 0) -> HourFrequency {
-        self
+        .init(value: self.value, min: min, sec: sec)
     }
 }
 
 public struct MinuteFrequency: Frequency {
     let value: Int64
+    
+    let sec: Int
+    
     public var time: TimeAmount { .minutes(value) }
     
+    public var initialDelay: TimeAmount {
+        self.timeUntilNext(sec: self.sec)
+    }
+    
     public func at(sec: Int = 0) -> MinuteFrequency {
-        self
+        .init(value: self.value, sec: sec)
     }
 }
 
 extension Int {
-    public var days: DayFrequency { DayFrequency(value: Int64(self)) }
-    public var hours: HourFrequency { HourFrequency(value: Int64(self)) }
-    public var minutes: MinuteFrequency { MinuteFrequency(value: Int64(self)) }
+    public var days: DayFrequency { DayFrequency(value: Int64(self), hr: 0, min: 0, sec: 0) }
+    public var hours: HourFrequency { HourFrequency(value: Int64(self), min: 0, sec: 0) }
+    public var minutes: MinuteFrequency { MinuteFrequency(value: Int64(self), sec: 0) }
 }
