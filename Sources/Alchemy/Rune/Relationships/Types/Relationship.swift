82573908b3c43713f54c92f1489e1cbbd40116a8
Josh Wright <joshuawright11@gmail.com>
2020-12-18 14:01:53 -0800

    Run: Part 2

--- Sources/Alchemy/Rune/Relationships/Types/Relationship.swift
+++ Sources/Alchemy/Rune/Relationships/Types/Relationship.swift
@@ -1,5 +1,7 @@
 import NIO
 
+/// A protocol representing a relationship between two `Model`s. Contains only those two types and
+/// functionality for eager loading this relationship.
 public protocol Relationship {
     associatedtype From: Model
     associatedtype To: RelationAllowed
@@ -9,4 +11,24 @@ public protocol Relationship {
         with nestedQuery: @escaping (ModelQuery<To.Value>) -> ModelQuery<To.Value>,
         from eagerLoadKeyPath: KeyPath<From, Self>
     ) -> EventLoopFuture<[From]>
+}
+
+/// This protocol exists
+public protocol RelationAllowed {
+    associatedtype Value: Model
+    var elementType: Value.Type { get }
+
+    static func from(_ value: Value?) throws -> Self
+}
+
+extension Model where Value == Self {
+    public var elementType: Self.Type { Self.self }
+    public static func from(_ value: Value?) throws -> Self {
+        try value.unwrap(or: RuneError.relationshipWasNil)
+    }
+}
+
+extension Optional: RelationAllowed where Wrapped: Model {
+    public var elementType: Wrapped.Type { Wrapped.self }
+    public static func from(_ value: Wrapped?) throws -> Optional<Wrapped> { value }
 }
