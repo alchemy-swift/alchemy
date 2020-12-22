c19a6b032332f4f5ea1caf9fbd65f4c5a566eb36
Josh Wright <joshuawright11@gmail.com>
2020-12-22 00:50:48 -0800

    Rewrite & Document Fusion (#17)

--- Sources/Fusion/Container.swift
+++ Sources/Fusion/Container.swift
@@ -1,89 +1,166 @@
-public final class Container {
-    public static var global = Container()
-    
-    var storage: [String: FusableResolver] = [:]
-    
-    private func register<T: IdentifiedService>(singleton: T, identifier: T.Identifier) throws {
-        let key = self.key(for: T.self)
-        if let existingValue = self.storage[key] {
-            guard let existingValue = existingValue as? IdentifiedSingletonResolver else {
-                throw FusionError.registeredServiceResolverMismatch
-            }
-
-            guard existingValue.values[identifier] == nil else {
-                throw FusionError.serviceAlreadyRegistered
-            }
-            
-            self.storage[key] = existingValue.adding(service: singleton, for: identifier)
-        } else {
-            self.storage[key] = IdentifiedSingletonResolver(values: [identifier: singleton])
-        }
-    }
-    
-    // This closure will be called once, when the service is first registered.
-    private func register<T: SingletonService>(singleton: T) throws {
-        let key = self.key(for: T.self)
-        guard self.storage[key] == nil else {
-            throw FusionError.serviceAlreadyRegistered
-        }
-        self.storage[key] = SingletonResolver(value: singleton)
-    }
-
-    // This closure will be called each time a service is "Fused" with `@Inject`.
-    private func register<T: FactoryService>(factory: @escaping (Container) throws -> T) throws {
-        let key = self.key(for: T.self)
-        guard self.storage[key] == nil else {
-            throw FusionError.serviceAlreadyRegistered
-        }
-        self.storage[key] = FactoryResolver { [weak self] in
-            guard let this = self else {
-                throw FusionError.containerDeallocated
-            }
-            
-            return try factory(this)
-        }
-    }
-    
-    public func resolve<T: IdentifiedService>(_ type: T.Type = T.self, identifier: T.Identifier) throws -> T {
-        let key = self.key(for: T.self)
-        guard let value = self.storage[key] else {
-            let singleton = try T.singleton(in: self, for: identifier)
-            try self.register(singleton: singleton, identifier: identifier)
-            return singleton
-        }
-        
-        return try value.getTypedValue(for: identifier)
-    }
-    
-    public func resolve<T: SingletonService>(_ type: T.Type = T.self) throws -> T {
-        let key = self.key(for: type)
-        guard let value = self.storage[key] else {
-            let singleton = try T.singleton(in: self)
-            try self.register(singleton: singleton)
-            return singleton
-        }
-        
-        return try value.getTypedValue(for: nil)
-    }
-    
-    public func resolve<T: FactoryService>(_ type: T.Type = T.self) throws -> T {
-        let key = self.key(for: type)
-        guard let value = self.storage[key] else {
-            let factory = T.factory
-            try self.register(factory: factory)
-            return try factory(self)
-        }
-        
-        return try value.getTypedValue(for: nil)
-    }
-    
-    private func key<T: Fusable>(for type: T.Type) -> String {
-        String(reflecting: type)
-    }
+/// The caching behavior, per container, for a factory.
+private enum ResolveBehavior {
+    /// A new instance should be created once per container.
+    case singleton
+    /// A new instance should be created at every `.resolve(...)`.
+    case transient
 }
 
-extension SingletonService {
-    static func resolve(from container: Container) throws -> Self {
-        try container.resolve(Self.self)
+/// A container from which services should be registered and resolved.
+public final class Container {
+    /// Generic factory closure. A container and an optional identifier are passed in and a service
+    /// is generated.
+    private typealias FactoryClosure = (Container, Any?) -> Any
+    
+    /// A global, singleton container.
+    public static var global = Container()
+    
+    /// The parent container of this container. Resolves that don't have a value in this container
+    /// will be deffered to the parent container.
+    private var parent: Container?
+    
+    /// Any cached instances of services held in this container (used for singletons and multitons)
+    private var instances: [String: Any] = [:]
+    
+    /// The resolvers registered to this container. Each resolver has a factory closure and behavior
+    /// by which the values are cached or not.
+    private var resolvers: [String: (behavior: ResolveBehavior, factory: FactoryClosure)] = [:]
+    
+    /// Initialize a container with an optional parent `Container`.
+    ///
+    /// - Parameter parent: the optional parent `Container`. Defaults to nil.
+    public init(parent: Container? = nil) {
+        self.parent = parent
+    }
+    
+    /// Register a transient service to this container. Transient means that it's factory closure
+    /// will be called _each time_ the service type is resolved.
+    ///
+    /// - Parameters:
+    ///   - service: the type of the service to register.
+    ///   - factory: the closure for instantiating an instance of the service.
+    public func register<T>(_ service: T.Type, factory: @escaping (Container) -> T) {
+        let key = self.storageKey(for: service, identifier: nil)
+        self.resolvers[key] = (.transient, { container, _ in
+            factory(container)
+        })
+    }
+    
+    /// Register a singleton service to this container. Singleton means that it's factory closure
+    /// will be called _once_ and that value will be returned each time the service is resolved.
+    ///
+    /// - Parameters:
+    ///   - service: the type of the service to register.
+    ///   - factory: the closure for instantiating an instance of the service.
+    public func register<S>(singleton service: S.Type, factory: @escaping (Container) -> S) {
+        let key = self.storageKey(for: service, identifier: nil)
+        self.resolvers[key] = (.singleton, { container, _ in
+            factory(container)
+        })
+    }
+    
+    /// Register a multiton service to this container. MultitonService is like a singleton except
+    /// that the factory closure is called once _per identifier value_ of the `MultitonService`.
+    /// When a multiton is resolved with an id value for the first time, a new instance of the
+    /// service is created and associated with that id value. Subsequent resolves with that id will
+    /// return the created value.
+    ///
+    /// See `MultitonService`.
+    ///
+    /// - Parameters:
+    ///   - service: the type of the service to register.
+    ///   - factory: the closure for instantiating an instance of the service.
+    public func register<M: MultitonService>(
+        multiton service: M.Type,
+        factory: @escaping (Container, M.Identifier) -> M
+    ) {
+        let key = self.storageKey(for: service, identifier: nil)
+        self.resolvers[key] = (.singleton, { factory($0, $1 as! M.Identifier) })
+    }
+    
+    /// Resolves a service, returning an instance of it.
+    ///
+    /// - Parameter service: the type of the service to resolve.
+    /// - Returns: an instance of the service.
+    public func resolve<T>(_ service: T.Type) -> T {
+        self.assertNotNil(self._resolve(service, identifier: nil))
+    }
+    
+    /// Resolves a `MultitonService`, returning an instance associated with the given identifier.
+    ///
+    /// - Parameters:
+    ///   - service: the type of the service to resolve.
+    ///   - identifier: the identifier for resolving this `MultitonService`.
+    /// - Returns: an instance of this `MultitonService` for the given identifier.
+    public func resolve<M: MultitonService>(multiton service: M.Type, identifier: M.Identifier) -> M {
+        self.assertNotNil(self._resolve(service, identifier: identifier))
+    }
+    
+    /// Resolves a generic service with an optional identifier.
+    ///
+    /// Internal for usage in the `Inject` property wrapper.
+    ///
+    /// - Parameters:
+    ///   - service: the type of the service to resolve.
+    ///   - identifier: an optional identifier that may be associated with this service.
+    /// - Returns: an instance of the service, if it is able to be resolved by this `Container` or
+    ///            it's parents.
+    func _resolve<T>(_ service: T.Type, identifier: AnyHashable?) -> T? {
+        let key = self.storageKey(for: service, identifier: identifier)
+        if let instance = self.instances[key] {
+            return self.assertType(of: instance)
+        } else if let resolver = self.resolvers[key] {
+            let instance: T = self.assertType(of: resolver.factory(self, identifier))
+            if resolver.behavior != .singleton {
+                self.instances[key] = instance
+            }
+            return instance
+        } else if let instance = self.parent?._resolve(service, identifier: identifier) {
+            return instance
+        } else {
+            return nil
+        }
+    }
+    
+    /// A key for local storage of instances and factories of services. It's the type name & the
+    /// hash value of the identifier (if there is one), separated by an underscore.
+    ///
+    /// - Parameters:
+    ///   - service: the service type to generate a key for.
+    ///   - identifier: any identifier that
+    /// - Returns: a string for keying the dictionaries that may hold instances or factories
+    ///            associated with the service type.
+    private func storageKey<T>(for service: T.Type, identifier: AnyHashable?) -> String {
+        var base = "\(service)"
+        if let identifier = identifier {
+            base += "_\(identifier.hashValue)"
+        }
+        return base
+    }
+    
+    /// Asserts that an optional value is not nil. If it is nil, a fatal error occurs.
+    ///
+    /// - Parameter value: the value to check for nil.
+    /// - Returns: the unwrapped value `T`.
+    private func assertNotNil<T>(_ value: T?) -> T {
+        guard let unwrapped = value else {
+            fatalError("Unable to resolve service of type \(T.self)! Perhaps it isn't registered?")
+        }
+        
+        return unwrapped
+    }
+    
+    /// Asserts that an instance matches another type. If it does not, a fatal error occurs.
+    ///
+    /// - Parameters:
+    ///   - instance: the instance to check the type of.
+    ///   - equals: the type to ensure `instance` conforms to.
+    /// - Returns: the instance cast to `U` if the conversion was successful.
+    private func assertType<T, U>(of instance: T, equals: U.Type = U.self) -> U {
+        guard let instance = instance as? U else {
+            fatalError("Internal storage type mismatch.")
+        }
+        
+        return instance
     }
 }
--- Sources/Fusion/Inject.swift
+++ Sources/Fusion/Inject.swift
@@ -1,47 +1,63 @@
+/// Conform a class to `Containerized` lets the `@Inject` property wrapper know that there is a
+/// custom container from which services should be resolved. If the enclosing type of the property
+/// wrapper is not `Containerized`, injected services will be resolved from `Container.global`.
+///
+/// Usage:
+/// ```
+/// class UsersController: Containerized {
+///     let container = Container()
+///
+///     // Will be resolved from `self.container` instead of `Container.global`
+///     @Inject var database: Database
+/// }
+/// ```
+public protocol Containerized: class {
+    /// The container from which `@Inject`ed services on this type should be resolved.
+    var container: Container { get }
+}
+
 @propertyWrapper
-public class Inject<Value: Fusable> {
-    // Just a single, global container for now. Would be great to have the user insert a custom container,
-    // ideally like @Inject(container: self.container) but there is not a way to access self in a property
-    // wrapper init, yet.
-    //
-    // See: https://stackoverflow.com/questions/58079611/access-self-in-swift-5-1-property-wrappers
-    //
-    // Maybe workaroud here?
-    // https://forums.swift.org/t/property-wrappers-access-to-both-enclosing-self-and-wrapper-instance/32526
-    private var container: Container = .global
-    private var storedValue: Value?
-    private var erasedResolver: (Container) throws -> Value
+/// Provides a convenient `@propertyWrapper` for injecting services to a type. By default, resolves
+/// services from the global container (`Container.global`) but if the enclosing type conforms to
+/// `Containerized` services are resolved from `EnclosingType.container`.
+public class Inject<Service> {
+    /// An optional identifier that may be associated with the service this property wrapper is
+    /// injecting. Used for storing any identifiers of a `MultitonService`.
+    var identifier: AnyHashable?
     
-    public var wrappedValue: Value {
+    /// This should never be called; `subscript` should intercept all calls through here.
+    public var wrappedValue: Service {
+        get { fatalError("This should never be called.") }
+        set { fatalError("This should never be called.") }
+    }
+    
+    /// Create the property wrapper with no identifier.
+    public init() {}
+    
+    /// Resolves an instance of `Service` from the given container.
+    ///
+    /// - Parameter container: the container to resolve a `Service` from.
+    /// - Returns: an instance of `Service` resolved from `container`.
+    private func resolve(in container: Container) -> Service {
+        container._resolve(Service.self, identifier: self.identifier)!
+    }
+    
+    /// Leverages an undocumented `Swift` API for accessing the enclosing type of a property
+    /// wrapper to detect if the enclosing type is `Containerized` and then use that container for
+    /// resolving when the `wrappedValue` of the property wrapper is accessed.
+    public static subscript<EnclosingSelf>(
+        _enclosingInstance object: EnclosingSelf,
+        wrapped wrappedKeyPath: ReferenceWritableKeyPath<EnclosingSelf, Service>,
+        storage storageKeyPath: ReferenceWritableKeyPath<EnclosingSelf, Inject<Service>>
+    ) -> Service {
         get {
-            guard let storedValue = storedValue else {
-                // Someday, property wrappers will be able to throw and we won't need to fatal error out.
-                // Though a fatal error might not be a bad idea; the program probably shouldn't run with
-                // misconfigured dependencies.
-                do {
-                    return try self.initializeValue()
-                } catch {
-                    fatalError("Error resolving this service: \(error)")
-                }
-            }
-            
-            return storedValue
+            let customContainer = (object as? Containerized)?.container
+            return object[keyPath: storageKeyPath].resolve(in: customContainer ?? .global)
         }
-    }
-    
-    // Only here so the compiler doesn't hurt itself and require `()` after the `@Inject`. Convenience inits
-    // will override.
-    public init() {
-        fatalError("This should never be called.")
-    }
-    
-    init(erasedResolver: @escaping (Container) throws -> Value) {
-        self.erasedResolver = erasedResolver
-    }
-    
-    private func initializeValue() throws -> Value {
-        let value = try self.erasedResolver(self.container)
-        self.storedValue = value
-        return value
+        set {
+            // This setter is needed so that the propert wrapper will have a `WritableKeyPath` for
+            // using this subscript.
+            fatalError("Services should not be set.")
+        }
     }
 }
