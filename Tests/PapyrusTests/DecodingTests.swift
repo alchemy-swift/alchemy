1e7a1623a8f9b45cee76dabe831aff0c58831821
Josh Wright <joshuawright11@gmail.com>
2020-12-29 22:32:12 -0800

    Flesh out failing tests

--- Tests/PapyrusTests/DecodingTests.swift
+++ Tests/PapyrusTests/DecodingTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import Papyrus
 
 final class DecodingTests: XCTestCase {
-    func testExample() throws {
+    func testDecodeRequest() throws {
         let decodedRequest = try TestRequest(from: MockRequest())
         XCTAssertEqual(decodedRequest.header1, "header1")
         XCTAssertEqual(decodedRequest.path1, "path1")
@@ -14,23 +14,49 @@ final class DecodingTests: XCTestCase {
         XCTAssertEqual(decodedRequest.body.string, "test")
         XCTAssertEqual(decodedRequest.body.int, 0)
     }
+    
+    func testDecodeURLBody() throws {
+        let decodedRequest = try TestURLBody(from: MockRequest())
+        XCTAssertEqual(decodedRequest.body.string, "test1")
+        XCTAssertEqual(decodedRequest.body.int, 1)
+    }
+    
+    func testDecodeMultipleBodyThrows() {
+        XCTAssertThrowsError(try MultipleBodies(from: MockRequest()))
+    }
 }
 
 struct MockRequest: DecodableRequest {
+    let headers: [String: String]
+    let paths: [String: String]
+    let queries: [String: String]
+    let bodyData: Data?
+    
+    init(
+        headers: [String: String] = [:],
+        paths: [String: String] = [:],
+        queries: [String: String] = [:],
+        bodyData: Data? = nil
+    ) {
+        self.headers = headers
+        self.paths = paths
+        self.queries = queries
+        self.bodyData = bodyData
+    }
+    
     func getHeader(for key: String) throws -> String {
-        key
+        self.headers[key] ?? ""
     }
     
     func getQuery(for key: String) throws -> String {
-        key
+        self.queries[key] ?? ""
     }
     
     func getPathComponent(for key: String) throws -> String {
-        key
+        self.paths[key] ?? ""
     }
     
     func getBody<T>() throws -> T where T : Decodable {
-        let data = try JSONEncoder().encode(SomeJSON(string: "test", int: 0))
-        return try JSONDecoder().decode(T.self, from: data)
+        try JSONDecoder().decode(T.self, from: self.bodyData ?? Data())
     }
 }
