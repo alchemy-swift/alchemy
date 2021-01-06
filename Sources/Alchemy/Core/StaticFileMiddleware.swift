6a7abf0e7b760fb1edf1c7d71d3af53612e9fff1
Josh Wright <joshuawright11@gmail.com>
2021-01-05 17:27:53 -0800

    Docs

--- Sources/Alchemy/Core/StaticFileMiddleware.swift
+++ Sources/Alchemy/Core/StaticFileMiddleware.swift
@@ -55,53 +55,56 @@ public struct StaticFileMiddleware: Middleware {
             }
             
             let fileHandle = try NIOFileHandle(path: filePath)
-            let response = EventLoopFuture<Response>.new(
-                Response { responseWriter in
-                    var headers: HTTPHeaders = ["content-length": "\(fileSizeBytes)"]
-                    if let ext = filePath.components(separatedBy: ".").last,
-                       let mediaType = MediaType(fileExtension: ext) {
-                        headers.add(name: "content-type", value: mediaType.value)
+            let response = Response { responseWriter in
+                // Set any relevant headers based off the file info.
+                var headers: HTTPHeaders = ["content-length": "\(fileSizeBytes)"]
+                if let ext = filePath.components(separatedBy: ".").last,
+                   let mediaType = MediaType(fileExtension: ext) {
+                    headers.add(name: "content-type", value: mediaType.value)
+                }
+                responseWriter.writeHead(status: .ok, headers)
+                
+                // Load the file in chunks, streaming it.
+                self.fileIO.readChunked(
+                    fileHandle: fileHandle,
+                    byteCount: fileSizeBytes,
+                    chunkSize: NonBlockingFileIO.defaultChunkSize,
+                    allocator: self.bufferAllocator,
+                    eventLoop: Loop.current,
+                    chunkHandler: { buffer in
+                        responseWriter.writeBody(buffer)
+                        return .new(())
                     }
-                    
-                    responseWriter.writeHead(status: .ok, headers)
-                    
-                    // Load the file in chunks, streaming it.
-                    self.fileIO.readChunked(
-                        fileHandle: fileHandle,
-                        byteCount: fileSizeBytes,
-                        chunkSize: NonBlockingFileIO.defaultChunkSize,
-                        allocator: self.bufferAllocator,
-                        eventLoop: Loop.current,
-                        chunkHandler: { buffer in
-                            responseWriter.writeBody(buffer)
-                            return .new(())
-                        }
-                    )
-                    .flatMapThrowing {
-                        try fileHandle.close()
-                    }
-                    .whenComplete { result in
-                        try? fileHandle.close()
-                        switch result {
-                        case .failure(let error):
-                            // Not a ton that can be done in the case of an error, not sure what
-                            // else can be done besides logging and ending the request.
-                            Log.error("Encountered an error loading a static file: \(error)")
-                            responseWriter.writeEnd()
-                        case .success:
-                            responseWriter.writeEnd()
-                        }
+                )
+                .flatMapThrowing {
+                    try fileHandle.close()
+                }
+                .whenComplete { result in
+                    try? fileHandle.close()
+                    switch result {
+                    case .failure(let error):
+                        // Not a ton that can be done in the case of an error, not sure what
+                        // else can be done besides logging and ending the request.
+                        Log.error("Encountered an error loading a static file: \(error)")
+                        responseWriter.writeEnd()
+                    case .success:
+                        responseWriter.writeEnd()
                     }
                 }
-            )
+            }
             
-            return response
+            return .new(response)
         } else {
             // No file, continue to handlers.
             return next(request)
         }
     }
     
+    /// Sanitize a file path, returning the new sanitized path.
+    ///
+    /// - Parameter path: The path to sanitize for file access.
+    /// - Throws: An error if the path is forbidden.
+    /// - Returns: The sanitized path, appropriate for loading files from.
     private func sanitizeFilePath(_ path: String) throws -> String {
         var sanitizedPath = path
         
