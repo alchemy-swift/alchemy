b9526052c205d1fc46f4182738bb6e227af323a2
Josh Wright <joshuawright11@gmail.com>
2021-01-07 00:09:10 -0800

    Clean up exports

--- Sources/Alchemy/Exports.swift
+++ Sources/Alchemy/Exports.swift
@@ -1,8 +1,41 @@
 // These libraries are also imported wherever Alchemy is.
-@_exported import NIO
-@_exported import NIOHTTP1
-@_exported import AsyncKit
-@_exported import Foundation
+
+// Alchemy related
 @_exported import Fusion
 @_exported import Papyrus
+
+// AsyncHTTPClient
+@_exported import class AsyncHTTPClient.HTTPClient
+@_exported import struct AsyncHTTPClient.HTTPClientError
+
+// Foundation
+@_exported import Foundation
+
+// Lifecycle
+@_exported import Lifecycle
+
+// Logging
+@_exported import Logging
+
+// NIO
+@_exported import struct NIO.ByteBuffer
+@_exported import struct NIO.ByteBufferAllocator
+@_exported import class NIO.EmbeddedEventLoop
+@_exported import protocol NIO.EventLoop
+@_exported import class NIO.EventLoopFuture
+@_exported import protocol NIO.EventLoopGroup
+@_exported import struct NIO.EventLoopPromise
+@_exported import class NIO.MultiThreadedEventLoopGroup
+@_exported import struct NIO.NonBlockingFileIO
+@_exported import class NIO.NIOThreadPool
+@_exported import enum NIO.System
+@_exported import struct NIO.TimeAmount
+
+// NIOHTTP1
+@_exported import struct NIOHTTP1.HTTPHeaders
+@_exported import enum NIOHTTP1.HTTPMethod
+@_exported import struct NIOHTTP1.HTTPVersion
+@_exported import enum NIOHTTP1.HTTPResponseStatus
+
+// Plot
 @_exported import Plot
