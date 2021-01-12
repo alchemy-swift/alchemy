33756640b4c046115ff832e546522a689a8bac95
Josh Wright <joshuawright11@gmail.com>
2021-01-12 14:16:39 -0800

    Update backend with complete endpoint

--- Quickstarts/Backend/Sources/Backend/Controllers/TodoController.swift
+++ Quickstarts/Backend/Sources/Backend/Controllers/TodoController.swift
@@ -21,7 +21,6 @@ struct TodoController: Controller {
                     .with(\.$tags)
                     .allModels()
             }
-            
             // Create a todo, with tags
             .post("/todo") { req -> EventLoopFuture<Todo> in
                 let user = try req.get(User.self)
@@ -44,7 +43,6 @@ struct TodoController: Controller {
                             .map { _ in todo }
                     }
             }
-            
             // Delete a todo
             .delete("/todo/:todoID") { request -> EventLoopFuture<Void> in
                 let userID = try request.get(User.self).getID()
@@ -57,7 +55,7 @@ struct TodoController: Controller {
                 // that only the owner of the `Todo` can delete
                 // it).
                 return Todo.query()
-                    .where("id" == Int(todoID))
+                    .where("id" == todoID)
                     .where("user_id" == userID)
                     .firstModel()
                     // Unwrap it, or return a 404 if it wasn't found.
@@ -71,6 +69,30 @@ struct TodoController: Controller {
                             .delete()
                             // Then, delete the todo itself.
                             .flatMap { _ in todo.delete() }
+                    }
+            }
+            // Complete a Todo
+            .patch("/todo/:todoID") { request -> EventLoopFuture<Todo> in
+                let userID = try request.get(User.self).getID()
+                // Fetch the relevant path component...
+                let idString = try request.pathComponent(for: "todoID")
+                    .unwrap(or: HTTPError(.badRequest))
+                let todoID = try Int(idString)
+                    .unwrap(or: HTTPError(.badRequest))
+                // Find the `Todo` with the given ID & userID (so that
+                // only the owner of the `Todo` can complete it).
+                return Todo.query()
+                    .where("id" == todoID)
+                    .where("user_id" == userID)
+                    .firstModel()
+                    // Unwrap it, or return a 404 if it wasn't found.
+                    .unwrap(orError: HTTPError(.notFound))
+                    // Toggle the Todo's completion status, then save
+                    // it.
+                    .flatMap { todo -> EventLoopFuture<Todo> in
+                        var updated = todo
+                        updated.isComplete.toggle()
+                        return updated.save()
                     }
             }
     }
