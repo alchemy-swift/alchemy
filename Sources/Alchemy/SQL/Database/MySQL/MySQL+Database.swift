df73ee59146e8785296986748e9ccc6b072f255d
Chris Anderson <hi@chrisanderson.io>
2021-01-19 11:02:12 -0600

    Ghetto temporary solution for getting previous row

--- Sources/Alchemy/SQL/Database/MySQL/MySQL+Database.swift
+++ Sources/Alchemy/SQL/Database/MySQL/MySQL+Database.swift
@@ -42,14 +42,47 @@ public final class MySQLDatabase: Database {
         )
     }
     
-    public func runRawQuery(_ sql: String, values: [DatabaseValue]) -> EventLoopFuture<[DatabaseRow]> {
+    public func runRawQuery(
+        _ sql: String,
+        values: [DatabaseValue]
+    ) -> EventLoopFuture<[DatabaseRow]> {
         self.pool.withConnection(logger: Log.logger, on: Services.eventLoop) { conn in
-            conn.query(sql, values.map(MySQLData.init))
+
+            var lastInsertId: Int?
+            return conn.query(
+                sql, values.map(MySQLData.init),
+                onMetadata: { metadata in
+                    if let lastId = metadata.lastInsertID {
+                        lastInsertId = Int(lastId)
+                    }
+                })
+                .flatMap { (rows: [MySQLRow]) -> EventLoopFuture<[MySQLRow]> in
+                    if let lastTable = self.tableMatches(query: sql),
+                       let lastInsertId = lastInsertId {
+                        return self.getLastInsertedRow(conn, table: lastTable, id: lastInsertId)
+                    }
+                    return EventLoopFuture<[MySQLRow]>.new(rows)
+                }
                 .map { $0 }
         }
     }
-    
+
     public func shutdown() throws {
         try self.pool.syncShutdownGracefully()
+    }
+
+    private func getLastInsertedRow(_ conn: MySQLConnection, table: String, id: Int) -> EventLoopFuture<[MySQLRow]> {
+        let bindings = [DatabaseValue.int(id)].map(MySQLData.init)
+        return conn.query("select * from \(table) where id = ?;", bindings)
+    }
+
+    private func tableMatches(query: String) -> String? {
+        let pattern = "^insert into ([^ ]*)"
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: [.caseInsensitive]) else { return nil }
+        let matches = regex.matches(in: query, options: [], range: NSMakeRange(0, query.count))
+        return matches.map { match in
+            let range = Range(match.range(at: match.numberOfRanges - 1), in: query)
+            return String(query[range!])
+        }.first
     }
 }
