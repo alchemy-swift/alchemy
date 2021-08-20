e9a043526d0a653a6a7abf6429718ab9097699c2
Josh Wright <joshuawright11@gmail.com>
2021-08-20 16:25:05 -0700

    Cleanup and fix cron compiler warnings

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.4
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -15,19 +13,18 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.1.0"),
         .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0-rc.2.2"),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.11.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/postgres-kit", from: "2.0.0"),
         .package(url: "https://github.com/vapor/mysql-kit", from: "4.0.0-rc.1.6"),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
-        .package(name: "Plot", url: "https://github.com/johnsundell/plot.git", from: "0.8.0"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha"),
-        .package(url: "https://github.com/Mordil/RediStack.git", from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
         .package(url: "https://github.com/alchemy-swift/papyrus", .branch("main")),
         .package(url: "https://github.com/alchemy-swift/fusion", .branch("main")),
-        .package(name: "Cron", url: "https://github.com/Ponyboy47/Cron-Swift.git", from: "2.3.0"),
+        .package(url: "https://github.com/alchemy-swift/cron.git", from: "2.3.2"),
+        .package(name: "Plot", url: "https://github.com/johnsundell/plot.git", from: "0.8.0"),
+        .package(url: "https://github.com/Mordil/RediStack.git", from: "1.0.0"),
     ],
     targets: [
         .target(
@@ -42,14 +39,13 @@ let package = Package(
                 .product(name: "MySQLNIO", package: "mysql-nio"),
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
-                .product(name: "NIOHTTP2", package: "swift-nio-http2"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "Plot", package: "Plot"),
                 .product(name: "LifecycleNIOCompat", package: "swift-service-lifecycle"),
                 .product(name: "RediStack", package: "RediStack"),
                 .product(name: "Papyrus", package: "papyrus"),
                 .product(name: "Fusion", package: "fusion"),
-                .product(name: "Cron", package: "Cron"),
+                .product(name: "Cron", package: "cron"),
                 
                 /// Internal dependencies
                 "CAlchemy",
