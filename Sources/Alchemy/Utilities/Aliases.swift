e7f4e3c2f6e1810e13f95d61ee497c42e96813e2
Josh Wright <joshuawright11@gmail.com>
2021-11-24 00:14:28 -0800

    Update workflows and move Aliases

--- Sources/Alchemy/Utilities/Aliases.swift
+++ Sources/Alchemy/Utilities/Aliases.swift
@@ -1,7 +1,9 @@
+// The default configured Client
 public var Http: Client {
     Container.resolve(Client.self)
 }
 
+// The default configured Database
 public var DB: Database {
     Container.resolve(Database.self)
 }
