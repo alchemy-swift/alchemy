77d970eac102cb2759850fb898a4393888d77ff9
Josh Wright <joshuawright11@gmail.com>
2021-12-21 15:25:06 -0500

    Update locking

--- Sources/Alchemy/Queue/JobEncoding/JobDecoding.swift
+++ Sources/Alchemy/Queue/JobEncoding/JobDecoding.swift
@@ -1,16 +1,22 @@
+import NIOConcurrencyHelpers
+
 /// Storage for `Job` decoding behavior.
 struct JobDecoding {
-    @Locked static var registeredJobs: [Job.Type] = []
+    static var registeredJobs: [Job.Type] = []
     
     /// Stored decoding behavior for jobs.
-    @Locked private static var decoders: [String: (JobData) throws -> Job] = [:]
+    private static var decoders: [String: (JobData) throws -> Job] = [:]
+    
+    private static let lock = Lock()
     
     /// Register a job to cache its decoding behavior.
     ///
     /// - Parameter type: A job type.
     static func register<J: Job>(_ type: J.Type) {
-        decoders[J.name] = { try J(jsonString: $0.json) }
-        registeredJobs.append(type)
+        lock.withLock {
+            decoders[J.name] = { try J(jsonString: $0.json) }
+            registeredJobs.append(type)
+        }
     }
     
     /// Indicates if the given type is already registered.
@@ -18,7 +24,9 @@ struct JobDecoding {
     /// - Parameter type: A job type.
     /// - Returns: Whether this job type is already registered.
     static func isRegistered<J: Job>(_ type: J.Type) -> Bool {
-        decoders[J.name] != nil
+        lock.withLock {
+            decoders[J.name] != nil
+        }
     }
     
     /// Decode a job from the given job data.
@@ -27,21 +35,25 @@ struct JobDecoding {
     /// - Throws: Any errors encountered while decoding the job.
     /// - Returns: The decoded job.
     static func decode(_ jobData: JobData) throws -> Job {
-        guard let decoder = JobDecoding.decoders[jobData.jobName] else {
-            Log.warning("Unknown job of type '\(jobData.jobName)'. Please register it via `app.registerJob(\(jobData.jobName).self)`.")
-            throw JobError.unknownType
-        }
-        
-        do {
-            return try decoder(jobData)
-        } catch {
-            Log.error("[Queue] error decoding job named \(jobData.jobName). Error was: \(error).")
-            throw error
+        try lock.withLock {
+            guard let decoder = decoders[jobData.jobName] else {
+                Log.warning("Unknown job of type '\(jobData.jobName)'. Please register it via `app.registerJob(\(jobData.jobName).self)`.")
+                throw JobError.unknownType
+            }
+            
+            do {
+                return try decoder(jobData)
+            } catch {
+                Log.error("[Queue] error decoding job named \(jobData.jobName). Error was: \(error).")
+                throw error
+            }
         }
     }
     
     static func reset() {
-        decoders = [:]
-        registeredJobs = []
+        lock.withLock {
+            decoders = [:]
+            registeredJobs = []
+        }
     }
 }
--- Sources/Alchemy/Utilities/Locked.swift
+++ Sources/Alchemy/Utilities/Locked.swift
@@ -1,27 +1,20 @@
 import Foundation
+import NIOConcurrencyHelpers
 
-/// Used for providing thread safe access to a property.
+/// Used for providing thread safe access to a property. Doesn't work on
+/// collections.
 @propertyWrapper
 public struct Locked<T> {
     /// The threadsafe accessor for this property.
     public var wrappedValue: T {
-        get {
-            self.lock.lock()
-            defer { self.lock.unlock() }
-            return self.value
-        }
-        set {
-            self.lock.lock()
-            defer { self.lock.unlock() }
-            self.value = newValue
-        }
+        get { lock.withLock { value } }
+        set { lock.withLock { value = newValue } }
     }
     
     /// The underlying value of this property.
     private var value: T
-    
     /// The lock to protect this property.
-    private let lock = NSRecursiveLock()
+    private let lock = Lock()
     
     /// Initialize with the given value.
     ///
