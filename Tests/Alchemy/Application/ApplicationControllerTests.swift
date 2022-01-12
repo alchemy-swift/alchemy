bc8332145b1f862aa7882a347f8be8838da5a643
Josh Wright <joshuawright11@gmail.com>
2022-01-12 13:58:37 -0800

    Swap out expectatino

--- Tests/Alchemy/Application/ApplicationControllerTests.swift
+++ Tests/Alchemy/Application/ApplicationControllerTests.swift
@@ -8,17 +8,25 @@ final class ApplicationControllerTests: TestCase<TestApp> {
     }
     
     func testControllerMiddleware() async throws {
-        let exp1 = expectation(description: "")
-        let exp2 = expectation(description: "")
-        let exp3 = expectation(description: "")
+        actor ExpectActor {
+            var middleware1 = false, middleware2 = false, middleware3 = false
+            func mw1() async { middleware1 = true }
+            func mw2() async { middleware2 = true }
+            func mw3() async { middleware3 = true }
+        }
+        
+        let expect = ExpectActor()
         let controller = MiddlewareController(middlewares: [
-            ExpectMiddleware(expectation: exp1),
-            ExpectMiddleware(expectation: exp2),
-            ExpectMiddleware(expectation: exp3)
+            ActionMiddleware { await expect.mw1() },
+            ActionMiddleware { await expect.mw2() },
+            ActionMiddleware { await expect.mw3() }
         ])
         app.controller(controller)
         try await Test.get("/middleware").assertOk()
-        wait(for: [exp1, exp2, exp3], timeout: kMinTimeout)
+        
+        AssertTrue(await expect.middleware1)
+        AssertTrue(await expect.middleware2)
+        AssertTrue(await expect.middleware3)
     }
     
     func testControllerMiddlewareRemoved() async throws {
@@ -57,6 +65,15 @@ struct ExpectMiddleware: Middleware {
     
     func intercept(_ request: Request, next: (Request) async throws -> Response) async throws -> Response {
         expectation.fulfill()
+        return try await next(request)
+    }
+}
+
+struct ActionMiddleware: Middleware {
+    let action: () async -> Void
+    
+    func intercept(_ request: Request, next: (Request) async throws -> Response) async throws -> Response {
+        await action()
         return try await next(request)
     }
 }
