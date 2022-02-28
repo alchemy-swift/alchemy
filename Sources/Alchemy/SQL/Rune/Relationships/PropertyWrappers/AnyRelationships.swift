8f0315bf4e1d0b505e9be286923794f5ad88eea0
Josh Wright <joshuawright11@gmail.com>
2022-02-28 11:10:09 -0800

    WIP

--- Sources/Alchemy/SQL/Rune/Relationships/PropertyWrappers/AnyRelationships.swift
+++ Sources/Alchemy/SQL/Rune/Relationships/PropertyWrappers/AnyRelationships.swift
@@ -9,3 +9,32 @@ protocol AnyBelongsTo {
     
     init(from sqlValue: SQLValue?) throws
 }
+
+/*
+ Eager Loading
+ 1. Some model properties may store something in an SQL row that's a hook for
+    another object.
+    a. `BelongsTo` stores id, eager loads other `Model`
+    b. `Store` stores url, eager loads `File`
+    c. custom, load from SQValue & column, store as `SQLValue` at `String`
+    d. should all fields be ModelProperty? no; couldn't do multipler arrays.
+ 2.
+ */
+
+protocol ModelProperty {
+    // Initialize from a field.
+    init(field: SQLField)
+    
+    // Store as field.
+    func store() async throws
+}
+
+protocol EagerLoadable: ModelProperty {
+    // Eager loads this model, if it needs it.
+    func load(values: [SQLRow]) async throws
+}
+
+struct SQLField {
+    let column: String
+    let value: SQLValue
+}
