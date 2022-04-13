8526b3d0a84869734ce0658180fa26fc70c99569
Josh Wright <joshuawright11@gmail.com>
2022-04-12 18:39:29 -0700

    WIP

--- Sources/Alchemy/Notifications/SMS/SMSSender.swift
+++ Sources/Alchemy/Notifications/SMS/SMSSender.swift
@@ -1,20 +1,9 @@
 // MARK: SMS
 
-public var SMS: Notifier<SMSChannel> { .default }
-public func SMS(_ id: Notifier<SMSChannel>.Identifier) -> Notifier<SMSChannel> { .id(id) }
+public var SMS: SMSSender { .default }
+public func SMS(_ id: SMSSender.Identifier) -> SMSSender { .id(id) }
 
-public enum SMSChannel<Receiver: SMSReceiver>: Channel {
-    public typealias Message = SMSMessage
-}
-
-protocol Channel {
-    associatedtype M: Message
-    associatedtype R where M.R == R
-}
-
-public typealias SMSSender = Notifier<SMSChannel>
-
-public struct Notifier<C: Channel>: Service {
+public struct SMSSender: Service {
     public struct Identifier: ServiceIdentifier {
         private let hashable: AnyHashable
         public init(hashable: AnyHashable) { self.hashable = hashable }
@@ -39,6 +28,46 @@ public struct SMSPhoneNumber: SMSReceiver {
     public let phone: String
 }
 
-public protocol SMSProvider {
-    func send(message: String, to: String, from: String?) async throws
+//public protocol SMSProvider {
+//    func send(message: String, to: String, from: String?) async throws
+//}
+
+public protocol SMSProvider: ChannelProvider where M == SMSMessage<R>, R: SMSReceiver {}
+
+struct TwilioProvider<R: SMSReceiver>: SMSProvider {
+    func send(message: SMSMessage<R>, receiver: R) async throws {
+        // send
+    }
+}
+
+public protocol ChannelProvider {
+    associatedtype M: Message
+    associatedtype R: Receiver
+    
+    func send(message: M, receiver: R) async throws
+}
+
+public protocol NotificationChannel {
+    associatedtype Provider: ChannelProvider
+}
+
+public struct NotificationSender<Channel: NotificationChannel>: Service {
+    public struct Identifier: ServiceIdentifier {
+        private let hashable: AnyHashable
+        public init(hashable: AnyHashable) { self.hashable = hashable }
+    }
+    
+    let provider: Channel.Provider
+    
+    public init(provider: Channel.Provider) {
+        self.provider = provider
+    }
+    
+    public func send(message: Channel.Provider.M, receiver: Channel.Provider.R) async throws {
+        try await provider.send(message: message, receiver: receiver)
+    }
+}
+
+extension NotificationSender where Channel.Provider: SMSProvider {
+    
 }
