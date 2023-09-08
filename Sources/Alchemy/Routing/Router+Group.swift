c01a5d1dc14b9732c08fb020a589659da151a738
Josh Wright <joshuawright11@gmail.com>
2023-09-08 13:14:20 -0700

    Add additional middleware group functions

--- Sources/Alchemy/Routing/Router+Group.swift
+++ Sources/Alchemy/Routing/Router+Group.swift
@@ -2,8 +2,16 @@ extension Router {
     /// Creates a route group with the given prefix and middlewares. The prefix
     /// and middlewares will only be applied to routes defined on this
     /// specific group.
-    public func group(_ pathPrefix: String? = nil, middlewares: [Middleware] = []) -> Router {
-        appendGroup(prefixes: pathPrefix.map { [$0] } ?? [], middlewares: middlewares)
+    public func group(_ prefix: String? = nil, middlewares: [Middleware] = []) -> Router {
+        appendGroup(prefixes: prefix.map { [$0] } ?? [], middlewares: middlewares)
+    }
+
+    public func group(_ middlewares: Middleware...) -> Router {
+        appendGroup(prefixes: [], middlewares: middlewares)
+    }
+
+    public func group() -> Router {
+        appendGroup(prefixes: [], middlewares: [])
     }
 
     /// Groups a set of endpoints by a path prefix. All endpoints added in the
@@ -11,13 +19,13 @@ extension Router {
     /// middleware(s).
     ///
     /// - Parameters:
-    ///   - pathPrefix: The path prefix for all routes defined in `configure`.
+    ///   - prefix: The path prefix for all routes defined in `configure`.
     ///   - middlewares: Middlewares to apply to the group.
     ///   - configure: A closure for adding routes that will be prefixed by the
     ///     given path prefix and have the given middlewares applied.
     @discardableResult
-    public func grouping(_ pathPrefix: String? = nil, middlewares: [Middleware] = [], configure: (Router) -> Void) -> Self {
-        configure(group(pathPrefix, middlewares: middlewares))
+    public func grouping(_ prefix: String? = nil, middlewares: [Middleware] = [], configure: (Router) -> Void) -> Self {
+        configure(group(prefix, middlewares: middlewares))
         return self
     }
 
@@ -26,13 +34,13 @@ extension Router {
     /// run through the given middleware closure.
     ///
     /// - Parameters:
-    ///   - pathPrefix: The path prefix for all routes defined in `configure`.
+    ///   - prefix: The path prefix for all routes defined in `configure`.
     ///   - middleware: The middleware closure which will handle
     ///     all requests to this application.
     ///   - configure: A closure for adding endpoints that will be
     ///     intercepted by the given `Middleware`.
     @discardableResult
-    public func grouping(_ pathPrefix: String? = nil, middleware: @escaping Middleware.Handler, configure: (Router) -> Void) -> Self {
+    public func grouping(_ prefix: String? = nil, middleware: @escaping Middleware.Handler, configure: (Router) -> Void) -> Self {
         configure(group(middlewares: [AnonymousMiddleware(handler: middleware)]))
         return self
     }
